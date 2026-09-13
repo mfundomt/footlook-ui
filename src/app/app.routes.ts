84a5def485acafce94996ae04486194b905a46b4
mfundomt <mfundo1.mthembu@gmail.com>
2026-09-13 15:09:58 +0200

    refactor(routes): simplify routing configuration and remove unused components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/marketing/home/home/home'; 
-//import { HomeComponent } from './features/marketing/home/home.component';
-import { OverviewComponent } from './features/dashboard/overview/overview.component';
 
 export const routes: Routes = [
-	{ path: '', pathMatch: 'full', redirectTo: 'product' },
-	{ path: 'product', component: Home, title: 'FootLook | Request intelligence' },
-	{ path: 'developers', component: Home, title: 'FootLook | Developers' },
-	{ path: 'dashboard', component: OverviewComponent, title: 'FootLook | Overview' },
-	{ path: '**', redirectTo: 'product' },
+  { path: '', component: Home, title: 'FootLook | Request intelligence' },
+
+  { path: 'developers', component: Home, title: 'FootLook | Developers' },
+
+  { path: '**', redirectTo: '' },
 ];
